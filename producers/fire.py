fde1acdfbb0da52631f2a9b8e37cbc35e2052f80
PiyushVedwal7 <piyushvedwal11@gmail.com>
2024-10-19 22:52:14 +0530

    Initial commit

---
+++ producers/fire.py
@@ -0,0 +1,31 @@
+import random,time
+from kafka import KafkaProducer
+import json
+from datetime import datetime
+
+
+producer = KafkaProducer(
+    bootstrap_servers='localhost:9092',
+    value_serializer=lambda v: json.dumps(v).encode('utf-8')
+)
+
+
+fire_locations = ['Main Hall', 'Cafeteria', 'Library', 'Lab']
+entry_points = ['Main Gate', 'Back Entrance', 'Library Entrance', 'Lab Entrance']
+
+
+def events():
+    while True:
+        if random.random()<0.1:
+            location=random.choice(fire_locations)
+            alert_time=datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
+            fire_event={
+                "location": location,
+                "alert_type": "fire_detected",
+                "severity": random.choice(['low', 'medium', 'high']),
+                "time": alert_time
+
+            }
+    
+
+
